07875fc181cf627f25244106b0db61d8723137d4
SanQuan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-01-29 05:43:14 -0500

    Remove unused selector

--- src/state-provider/selectors.ts
+++ src/state-provider/selectors.ts
@@ -2,4 +2,3 @@ import { StateType } from "../types/types";
 
 export const selectModalContent = (state: StateType) => state.modalContent;
 export const selectSearchQuery = (state: StateType) => state.searchQuery;
-export const selectEntitiesList = (state: StateType) => state.entitiesList;
