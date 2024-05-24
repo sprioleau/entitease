7ee9cac021f221e3ab61b4cbe8bd39ff79f3952d
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-05-24 16:30:04 -0400

    🐛 Fix rendered content style

--- src/components/EntitiesList/index.tsx
+++ src/components/EntitiesList/index.tsx
@@ -39,68 +39,68 @@ export default function EntitiesList({ query }: Props) {
 		"filter-active": isFilterActive,
 	};
 
+	if (filteredEntities.length === 0) {
+		return (
+			<div className="entities-list no-results">
+				<h2 className="no-results-message">Sorry, no results for that search term.</h2>
+			</div>
+		);
+	}
+
 	return (
 		<ul
 			className="entities-list"
 			lang="en"
 		>
-			{filteredEntities.length > 0 ? (
-				<>
-					{filteredEntities.map((entityGroup: EntitityCategory) => (
-						<React.Fragment key={entityGroup.categoryLabel}>
-							<li className="entities-list__category">
-								<h3 className="entities-list__category-label">
-									{isFilterActive ? `Applied Filter: ${entityGroup.categoryLabel}` : entityGroup.categoryLabel}
-								</h3>
-								<span
-									className={composeClasses(arrowIconClasses)}
-									onClick={() => handleFilterByCategory(entityGroup.categoryLabel)}
-								>
-									<ArrowIcon />
-								</span>
-							</li>
-							{entityGroup?.entities?.map((entity, index) => (
-								<li
-									key={`${entityGroup.categoryLabel}-${entity.name}-${index}`}
-									className="entities-list__item"
-								>
-									<div className="entities-list__positioner">
-										<div className="entities-list__labels">
-											{Object.keys(entityDisplayValues).map((key) => {
-												const typedKey = key as keyof typeof entityDisplayValues;
+			{filteredEntities.map((entityGroup: EntitityCategory) => (
+				<React.Fragment key={entityGroup.categoryLabel}>
+					<li className="entities-list__category">
+						<h3 className="entities-list__category-label">
+							{isFilterActive ? `Applied Filter: ${entityGroup.categoryLabel}` : entityGroup.categoryLabel}
+						</h3>
+						<span
+							className={composeClasses(arrowIconClasses)}
+							onClick={() => handleFilterByCategory(entityGroup.categoryLabel)}
+						>
+							<ArrowIcon />
+						</span>
+					</li>
+					{entityGroup?.entities?.map((entity, index) => (
+						<li
+							key={`${entityGroup.categoryLabel}-${entity.name}-${index}`}
+							className="entities-list__item"
+						>
+							<div className="entities-list__positioner">
+								<div className="entities-list__labels">
+									{Object.keys(entityDisplayValues).map((key) => {
+										const typedKey = key as keyof typeof entityDisplayValues;
 
-												return (
-													<LabelWithCopy
-														key={key}
-														label={`${entityDisplayValues[typedKey].label}${entity[typedKey]}`}
-														tag={entityDisplayValues[typedKey].tag}
-														className="entities-list__item-label"
-														addClass={entityDisplayValues[typedKey].addClass}
-														textToCopy={entity[typedKey]}
-														isBlank={entity.isBlank}
-													/>
-												);
-											})}
-										</div>
-										<div className="entities-list__navigation">
-											<Icon
-												href={`/symbol/${dashString(entity.name)}`}
-												addClass="arrow-icon"
-												size="md"
-												icon={<ArrowIcon />}
+										return (
+											<LabelWithCopy
+												key={key}
+												label={`${entityDisplayValues[typedKey].label}${entity[typedKey]}`}
+												tag={entityDisplayValues[typedKey].tag}
+												className="entities-list__item-label"
+												addClass={entityDisplayValues[typedKey].addClass}
+												textToCopy={entity[typedKey]}
+												isBlank={entity.isBlank}
 											/>
-										</div>
-									</div>
-								</li>
-							))}
-						</React.Fragment>
+										);
+									})}
+								</div>
+								<div className="entities-list__navigation">
+									<Icon
+										href={`/symbol/${dashString(entity.name)}`}
+										addClass="arrow-icon"
+										size="md"
+										icon={<ArrowIcon />}
+									/>
+								</div>
+							</div>
+						</li>
 					))}
-				</>
-			) : (
-				<div className="entities-list no-results">
-					<h2 className="no-results-message">Sorry, no results for that search term.</h2>
-				</div>
-			)}
+				</React.Fragment>
+			))}
 		</ul>
 	);
 }
